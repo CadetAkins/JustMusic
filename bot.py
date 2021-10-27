e97ffc76f0c72794bfaa15b6a93116e93ceeac60
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-27 14:15:17 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,42 @@
+import os
+import discord
+from discord.ext import commands
+from replit import Database
+
+_db = Database(os.getenv("DB_URL"))
+
+class MusicBot(commands.Bot):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    self.db = _db
+
+bot = MusicBot(
+  command_prefix = ":",
+  intents = discord.Intents.default(),
+  help_command=None,
+  case_insensitive=True,
+  strip_after_whitespace=True
+)
+
+COGS = [
+  'plugins.core.errors.on_command_error', #error handler
+  'plugins.core.startup.login',
+  'plugins.core.help.help',
+  'plugins.music.playlists',
+  'plugins.music.music'
+]
+
+for cog in COGS:
+  bot.load_extension(cog)
+
+@bot.command()
+@commands.is_owner()
+async def wipe_db(ctx):
+  for key in _db.keys():
+    del _db[key]
+
+  await ctx.send("Database wiped.")
+
+
+bot.run(TOKEN)
