ed2ebd37fd914ae1099d9f973b17ad31e5b8334f
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-27 15:05:03 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from replit import Database
 
-_db = Database("https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2MzU0Nzc0NDUsImlhdCI6MTYzNTM2NTg0NSwiZGF0YWJhc2VfaWQiOiJjMDAxOTVkOS01M2Y5LTQ3OTctYmViOC1mOTIyNmU1NzQ2OTQifQ.TNoK4QS02sxsxBFTi6-KwYcEBDbCSrtzyeD4-gr8kWBk3bM6vUl1YOwgCM3y26o1DyInBXGy8us1_GeP1irN3w")
+_db = Database("")
 
 class MusicBot(commands.Bot):
   def __init__(self, *args, **kwargs):
@@ -39,4 +39,4 @@ async def wipe_db(ctx):
   await ctx.send("Database wiped.")
 
 
-bot.run("OTAyMzgzOTgzMzk0OTcxNjg4.YXdokw.22zMVbdAZC7qdkmqugL774H53kc")
+bot.run("")
